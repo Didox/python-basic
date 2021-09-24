edaa15d942924ce7a7f53d167cad896c0e521bd3
Danilo Santos <danilo.aparecido.santos@gmail.com>
2021-09-24 19:57:11 -0300

    python basic

---
+++ gato.py
@@ -0,0 +1,5 @@
+import animal
+
+class Gato(animal.Animal):
+  def __init__(self, nome, cor):
+    super().__init__(nome, cor)
---
+++ logica.py
@@ -0,0 +1,36 @@
+a = 1
+print(a)
+
+if a == 1 :
+  print("oláaaa")
+
+if a == 2 :
+  print("oláaaa")
+elif a == 1 :
+  print("olá else")
+else:
+  print("elseeee")
+
+
+lista = ["danilo", "aparecido", "santos"]
+for item in lista:
+    print("Item: " + item)
+    print(f"Item concat 2: {item}")
+
+
+print("Digite o seu nome")
+nome = input()
+print("Seu nome é: " + nome)
+
+
+print("Digite o sua idade")
+idade = int(input())
+idade += 1
+print("Sua idade + 1 é: " + str(idade))
+
+
+print("Qual valor")
+valor = float(input())
+valor += 1.8
+print("valor é: " + str(valor))
+
