687592309e8ef188e64694b67cdba18377e9896c
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-01-19 11:30:15 -0300

    basicos

---
+++ POO/cachorro.py
@@ -0,0 +1,7 @@
+
+
+import animal
+
+class Cachorro(animal.Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
---
+++ POO/coelho.py
@@ -0,0 +1,7 @@
+
+
+import animal
+
+class Coelho(animal.Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
